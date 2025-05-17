7876335f5d153f363f4fb444409f6f17e52e79e2
juraevboburjon <juraevboburjon73@gmail.com>
2025-05-17 16:46:32 +0900

    done nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = ({ userLoginned, formData }) => {
-  console.log(formData);
+  // console.log(formData);
   return (
     <div className="flex items-center justify-between w-full max-w-[1200px] h-20 mx-auto px-6">
       <div className="text-3xl font-black text-blue-800">Cool Pic</div>
