3fdaa0117b9f51d02512c930fbf858b2adea817b
juraevboburjon <juraevboburjon73@gmail.com>
2025-05-17 16:39:04 +0900

    create sign up

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -4,7 +4,7 @@ class AuthController {
   async register(req, res) {
     try {
       const user = await authService.register(req.body);
-      res.status(200).json(user);
+      res.status(200).json({ message: "User created success" });
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
@@ -13,9 +13,9 @@ class AuthController {
   async login(req, res) {
     try {
       const { token, username } = await authService.login(req.body);
-      res.status(200).json({ token, username });
+      res.status(200).json({ message: "User loginned success" });
     } catch (error) {
-      res.status(401).json({ message: error.message });
+      res.status(401).json({ message: "Email or Passwor not correct" });
     }
   }
 
