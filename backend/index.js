ab3da45d3dfcd63ee10cb239ffb6a3c443b7c268
juraevboburjon <juraevboburjon73@gmail.com>
2025-05-24 13:30:32 +0900

    config deployment vercel.json

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,7 @@ const commentRoute = require("./routes/comment.route");
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["https://cool-pic-pi.vercel.app", "http://localhost:5173"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 const PORT = process.env.PORT || 8000;
 
